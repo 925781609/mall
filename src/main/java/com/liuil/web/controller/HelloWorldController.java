612b6a2a1b6c7afc9787a11ca38c872daf4f3c61
WIN10-20170614A\LIU <yang.liu@hypers.com>
2017-12-30 15:32:23 +0800

    在application.properties中自定义属性，并绑定到属性字段上

--- src/main/java/com/liuil/web/controller/HelloWorldController.java
+++ src/main/java/com/liuil/web/controller/HelloWorldController.java
@@ -1,14 +1,20 @@
 package com.liuil.web.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+  @Value("${com.liuil.greeting}")
+  private String greeting;
+  @Value("${com.liuil.content}")
+  private String content;
+
   @RequestMapping("/hello")
-  public String index(){
-    return "Hello, My World";
+  public String greeting(){
+      return greeting + ", " + content;
   }
 
 }
