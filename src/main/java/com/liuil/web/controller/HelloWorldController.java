c07914f6ed6678e1eaccedc6c52682fd9ceb59ec
WIN10-20170614A\LIU <yang.liu@hypers.com>
2017-12-30 14:13:32 +0800

    新增HelloWorld及接口测试

---
+++ src/main/java/com/liuil/web/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.liuil.web.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+  @RequestMapping("/hello")
+  public String index(){
+    return "Hello, My World";
+  }
+
+}
