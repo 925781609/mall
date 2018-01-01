c3b7f22661d9b7e1d366feb98b88f6c8073791bc
WIN10-20170614A\LIU <yang.liu@hypers.com>
2018-01-01 11:23:25 +0800

    使用bootstrap+Thymeleaf编写的hello world

--- src/main/java/com/liuil/web/controller/HelloWorldController.java
+++ src/main/java/com/liuil/web/controller/HelloWorldController.java
@@ -1,21 +1,26 @@
 package com.liuil.web.controller;
 
 import com.liuil.web.domain.ConfigBean;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @EnableConfigurationProperties(ConfigBean.class)
 public class HelloWorldController {
 
   @Autowired
   ConfigBean configBean;
 
+
   @RequestMapping("/hello")
-  public String greeting(){
-      return configBean.getGreeting() + ", " + configBean.getContent();
+  public String welcome(Map<String, Object> model){
+      String message = configBean.getGreeting() + ", " + configBean.getContent();
+      model.put("message", message);
+      // return 的是template的名字
+      return "welcome1";
   }
 
 }
