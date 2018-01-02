bdac584763b25341cae2fbd06568067a17730c7f
WIN10-20170614A\LIU <yang.liu@hypers.com>
2018-01-02 20:41:06 +0800

    完善Mybatis和MySQL配置

--- src/main/java/com/liuil/web/WebApplication.java
+++ src/main/java/com/liuil/web/WebApplication.java
@@ -2,8 +2,10 @@ package com.liuil.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
+// mapper 接口类扫描包配置
+@MapperScan("org.spring.springboot.dao")
 public class WebApplication {
 
 	public static void main(String[] args) {
