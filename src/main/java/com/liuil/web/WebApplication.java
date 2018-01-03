e5d0614ba2ac9e850b34b542ca0d6cc06c647dc7
WIN10-20170614A\LIU <yang.liu@hypers.com>
2018-01-03 22:03:04 +0800

    refactor: 修改MapperScan位置

--- src/main/java/com/liuil/web/WebApplication.java
+++ src/main/java/com/liuil/web/WebApplication.java
@@ -2,10 +2,9 @@ package com.liuil.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 // mapper 接口类扫描包配置
-@MapperScan("com.liuil.web.dao")
+
 public class WebApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/liuil/web/service/impl/CityServiceImpl.java
+++ src/main/java/com/liuil/web/service/impl/CityServiceImpl.java
@@ -3,9 +3,11 @@ package com.liuil.web.service.impl;
 import com.liuil.web.dao.CityDao;
 import com.liuil.web.domain.City;
 import com.liuil.web.service.CityService;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 @Service
+@MapperScan("com.liuil.web.dao")
 public class CityServiceImpl implements CityService{
   @Autowired
   private CityDao cityDao;
