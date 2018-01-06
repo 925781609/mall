e2627349101e419a334e6b04bd55b6b0bb584743
yang.liu <yang.liu@hypers.com>
2018-01-06 11:22:39 +0800

    refactor: 删除mapper.xml多余的语句， 重构mapperscan的位置

--- src/main/java/com/liuil/web/WebApplication.java
+++ src/main/java/com/liuil/web/WebApplication.java
@@ -1,12 +1,13 @@
 package com.liuil.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
+
 // mapper 接口类扫描包配置
-
+@SpringBootApplication
+@MapperScan("com.liuil.web.dao")
 public class WebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
