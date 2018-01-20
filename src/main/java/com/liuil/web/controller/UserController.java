ec1a88d38a442e9f216aface7dd09b1c8c47ef5a
WIN10-20170614A\LIU <yang.liu@hypers.com>
2018-01-20 20:27:29 +0800

    feat: 增加日志用于调试，日志的配置还有待完善

--- src/main/java/com/liuil/web/controller/UserController.java
+++ src/main/java/com/liuil/web/controller/UserController.java
@@ -3,6 +3,7 @@ package com.liuil.web.controller;
 import com.liuil.web.service.UserService;
 import com.liuil.web.util.UserContext;
 import javax.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+  public static Logger logger = Logger.getLogger(UserController.class);
   // 定义一些模板常量
   public final static String LOGIN = "welcome1";
 
@@ -25,6 +27,8 @@ public class UserController {
   public String login(@RequestParam ("username") String username,
                        @RequestParam("pasword") String password,
                        HttpSession session){
+    logger.warn("in login put method");
+    logger.warn("username:" + username + "  " + "password" + password);
     try{
       if(userService.login(username, password)){
         System.out.println(username+ " " + password);
@@ -41,6 +45,8 @@ public class UserController {
   // 显示登入用的表单
   @RequestMapping(value="login", method=RequestMethod.GET)
   public String loginPage(HttpSession session){
+    logger.info("info");
+    logger.warn("warning");
     if(session.getAttribute(UserContext.USER_NAME) != null){
       return "redirect:/" ;
     }
