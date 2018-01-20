564305b9dedf08991c2d121df956a2f306d36e09
yang.liu <yang.liu@hypers.com>
2018-01-20 12:01:04 +0800

    feat: 使用modelAttribute的方式向model中添加key value数据

--- src/main/java/com/liuil/web/controller/HelloWorldController.java
+++ src/main/java/com/liuil/web/controller/HelloWorldController.java
@@ -5,7 +5,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @EnableConfigurationProperties(ConfigBean.class)
@@ -13,12 +15,20 @@ public class HelloWorldController {
 
   @Autowired
   ConfigBean configBean;
-
+  @ModelAttribute(value = "message")
+  public String addMessage(@RequestParam(required = false) String abc){
+    if(abc == null){
+      return "default";
+    }
+    else{
+      return abc;
+    }
+  }
 
   @RequestMapping("/hello")
   public String welcome(Map<String, Object> model){
       String message = configBean.getGreeting() + ", " + configBean.getContent();
-      model.put("message", message);
+      // model.put("message", message); # 上面的可以实现相同的功能
       // return 的是template的名字
       return "welcome1";
   }
