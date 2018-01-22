141bbeed899cedf47b7cc80defbf2f306496d160
yang.liu <925781609@qq.com>
2018-01-22 14:58:16 +0800

    refactor：重构用户表

--- mall.sql
+++ mall.sql
@@ -2,15 +2,16 @@
 --  用户表
 -- ----------------------------
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
+  `id` unsigned int(11) NOT NULL AUTO_INCREMENT COMMENT '用户id',
   `username` varchar(50) NOT NULL COMMENT '用户名',
   `password` varchar(50) NOT NULL COMMENT '用户密码，MD5加密',
   `email` varchar(50) DEFAULT NULL,
   `phone` varchar(20) DEFAULT NULL,
-  `role` int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
-  `confirmed` BOOLEAN DEFAULT FALSE COMMENT '邮箱验证',
-  `create_time` datetime NOT NULL COMMENT '创建时间',
-  `update_time` datetime NOT NULL COMMENT '最后一次更新时间',
+  `role` unsigned int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
+  `is_confirmed` unsigned tinyint DEFAULT 0 COMMENT '邮箱验证',
+  `is_active` unsigned tinyint DEFAULT 0 COMMENT '用户是否有效',
+  `gmt_create`   datetime NOT NULL COMMENT '创建时间',
+  `gmt_modified` datetime NOT NULL COMMENT '最后一次更新时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name_unique` (`username`) USING BTREE,
   UNIQUE KEY `email_unique` (`email`) USING BTREE,
