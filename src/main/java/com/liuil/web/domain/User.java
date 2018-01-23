9be7c38f4e0d08e2f685535d6179628da9394403
yang.liu <925781609@qq.com>
2018-01-23 18:34:25 +0800

    feat: 修改User Bean

--- src/main/java/com/liuil/web/domain/User.java
+++ src/main/java/com/liuil/web/domain/User.java
@@ -4,14 +4,15 @@ import java.util.Date;
 
 public class User {
   private int id;  // 自增ID
-  private String userName; // 用户名
+  private String username; // 用户名
   private String password; // 用户密码(MD5加密)
   private String email; // 邮箱(唯一)
   private String phone; // 手机号(唯一)
   private int   role; // 角色 0-管理员， 1-普通用户
   private boolean confirmed; // 是否通过邮箱验证
+  private boolean activate; // 用户是否有效
   private Date createTime; // 创建时间
-  private Date updateTime; // 更新时间
+  private Date modifiedTime; // 更新时间
 
   public int getId() {
     return id;
