cc51ae8c1133ce6648529c4cd34bdac390d2bbd2
WIN10-20170614A\LIU <yang.liu@hypers.com>
2018-01-23 21:26:20 +0800

    fix: 修改Login Interceptor

--- src/main/java/com/liuil/web/interceptor/LoginInterceptor.java
+++ src/main/java/com/liuil/web/interceptor/LoginInterceptor.java
@@ -4,15 +4,16 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Component
-public class LoginInterceptor implements HandlerInterceptor{
-  public static Logger logger = Logger.getLogger(HandlerInterceptor.getClass());
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+  //public static Logger logger = Logger.getLogger(LoginInterceptor.getClass());
 
   @Override
   public  boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws  Exception{
-    logger.info("preHandler is called");
+    //logger.info("preHandler is called");
+    return true;
   }
 
 }
