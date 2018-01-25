670cff3405d6dafd0215f015286752e1f398d79c
yang.liu <925781609@qq.com>
2018-01-25 17:19:29 +0800

    feat: 修改默认时间

--- mall.sql
+++ mall.sql
@@ -10,8 +10,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `role` unsigned int(4) NOT NULL COMMENT '角色0-管理员,1-普通用户',
   `is_confirmed` unsigned tinyint DEFAULT 0 COMMENT '邮箱验证',
   `is_active` unsigned tinyint DEFAULT 0 COMMENT '用户是否有效',
-  `gmt_create`   datetime NOT NULL COMMENT '创建时间',
-  `gmt_modified` datetime NOT NULL COMMENT '最后一次更新时间',
+  `gmt_create` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `gmt_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后一次更新时间',
+-- DEFAULT CURRENT_TIMESTAMP有问题，需要使用now() 插入当前时间
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_name_unique` (`username`) USING BTREE,
   UNIQUE KEY `email_unique` (`email`) USING BTREE,
